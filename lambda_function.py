6eb91ef9a662fb8868318d5186616ddcfadb4593
amarjeet63 <amarjeet63@outlook.com>
2024-03-18 16:45:13 +0530

    assigenment-commit-03

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,31 @@
 import json
-import requests
 import pandas as pd
+import boto3
+
+s3 = boto3.client('s3')
+
 
 def lambda_handler(event, context):
+    # TODO implement
+    # bucket = 'doordash-landing-zn01'
+    # key = '2024-03-01-raw_input.json'
+    print(event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
 
+    try:
+        data = s3.get_object(Bucket=bucket, Key=key)
+        json_data = []
+        json_data = data['Body'].read().decode('utf-8')
+        df = pd.Dataframe.from_dict(json_data)
+        print(json_data)
 
-    print("Event Data -> ", event)
-    response = requests.get("https://www.google.com/")
-    print(response.text)
+        print("Started Reading JSON file which contains multiple JSON document")
 
-    d ={'col1': [1,2], 'col2': [3,4]}
-    df=pd.DataFrame(data=d)
-    print(df)
-    print('Done Completed !!!')
+    except Exception as e:
+        raise e
 
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
